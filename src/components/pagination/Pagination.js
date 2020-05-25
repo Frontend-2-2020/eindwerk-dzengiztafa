54aa46f6e2898b9abc14aee328a69ca9af976b71
Dzengiz Tafa <dzengiz.tafa@gmail.com>
2020-05-25 20:18:57 +0200

    rearranged some code

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -15,48 +15,48 @@ import PageControls from "./page-controls/PageControls";
 
 const Pagination = ({ data, decrementPage, incrementPage, setPageEnd, setPageBegin, selectPage }) => {
 
-    let paginationContent = '';
+  let paginationContent = '';
 
-    // Hide the controls when there is no data
-    if (!data) {
-        return (
-            <div>{paginationContent}</div>
-        )
-    }
-
-    // Generate the pagination content
-    paginationContent = (
-        <div className="mb-2">
-            <PageNumbers
-                currentPage={data.current_page}
-                pages={data.last_page}
-                selectPage={selectPage}
-            />
-            <PageControls
-                decrementPage={decrementPage}
-                incrementPage={incrementPage}
-                setPageBegin={setPageBegin}
-                setPageEnd={setPageEnd}
-                selectPage={selectPage}/>
-        </div>
-    );
-
+  // Hide the controls when there is no data
+  if (!data) {
     return (
-        <div>
-            {paginationContent}
-        </div>
+      <div>{ paginationContent }</div>
     )
+  }
+
+  // Generate the pagination content
+  paginationContent = (
+    <div className="mb-2">
+      <PageNumbers
+        currentPage={ data.current_page }
+        pages={ data.last_page }
+        selectPage={ selectPage }
+      />
+      <PageControls
+        decrementPage={ decrementPage }
+        incrementPage={ incrementPage }
+        setPageBegin={ setPageBegin }
+        setPageEnd={ setPageEnd }
+        selectPage={ selectPage }/>
+    </div>
+  );
+
+  return (
+    <div>
+      { paginationContent }
+    </div>
+  )
 };
 
 
 // Prop types for the component
 Pagination.propTypes = {
-    data: PropTypes.object,
-    setPageEnd: PropTypes.func.isRequired,
-    setPageBegin: PropTypes.func.isRequired,
-    incrementPage: PropTypes.func.isRequired,
-    decrementPage: PropTypes.func.isRequired,
-    selectPage: PropTypes.func.isRequired
+  data: PropTypes.object,
+  setPageEnd: PropTypes.func.isRequired,
+  setPageBegin: PropTypes.func.isRequired,
+  incrementPage: PropTypes.func.isRequired,
+  decrementPage: PropTypes.func.isRequired,
+  selectPage: PropTypes.func.isRequired
 };
 
 
